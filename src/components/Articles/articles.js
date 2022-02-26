b83a6fdd0eb9dda99f28f2505cc7801341e8167d
Ilya Chvanov <mercy1ess@list.ru>
2022-02-26 12:52:16 +0300

    fetch api async/await try/catch/finnaly

--- src/components/Articles/articles.js
+++ src/components/Articles/articles.js
@@ -3,34 +3,34 @@ import { useEffect, useState } from "react";
 import { apiUrl } from "../../utils/constants";
 
 export const Articles = () => {
-  const [articles, serArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  useEffect(() => {
+  const getData = async () => {
     setIsLoading(true);
     setError(false);
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const result = await response.json();
+      setArticles(result);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-
-        return response.json();
-      })
-      .then((result) => serArticles(result))
-      .catch((err) => {
-        setError(true);
-        console.warn(err);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+  useEffect(() => {
+    getData();
   }, []);
 
   return (
     <>
+      <button onClick={getData}>Refresh</button>
       <h3>Articles</h3>
       {error && <h5>Error</h5>}
       {isLoading ? (
