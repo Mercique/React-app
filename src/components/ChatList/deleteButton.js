d22f42bcb603e39d6f8b4bfc2fcca9093ec3f32b
Ilya Chvanov <mercy1ess@list.ru>
2022-03-01 14:13:42 +0300

    fix problem with open messages form in deleted chat

--- src/components/ChatList/deleteButton.js
+++ src/components/ChatList/deleteButton.js
@@ -2,7 +2,8 @@ import { remove } from "firebase/database";
 import { getChatsRefById, getMessagesRefByChatId } from "../../services/firebase";
 
 export const DeleteButton = ({ id }) => {
-  const handleDeleteChat = () => {
+  const handleDeleteChat = (e) => {
+    e.preventDefault();
     remove(getChatsRefById(id));
     remove(getMessagesRefByChatId(id));
   };
